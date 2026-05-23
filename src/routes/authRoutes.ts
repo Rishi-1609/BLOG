84bcce76dab889762261402c44263377f73ea91d
rishindra <rishindra.rao.16@gmail.com>
2026-05-23 13:29:16 +0530

    Setup Auth Routes

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,65 @@
+import { Router } from 'express';
+import { register, login } from '../controllers/authControllers';
+import { ResolvePopulatedRawDocType } from 'mongoose';
+
+const router : Router = Router();
+
+/**
+ * @openapi
+ *
+ * /api/auth/register:
+ *   post:
+ *      summary: Register a new user
+ *      tags: [Auth]
+ *      requestBody: 
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                    type: object
+ *                    required: [name, email, password]
+ *                    properties: 
+ *                      name: 
+ *                        type: string
+ *                      email:
+ *                        type: string
+ *                      password: 
+ *                        type: string
+ *      responses: 
+ *        201: 
+ *          description: Created
+ *        400: 
+ *          description: Missing Fields
+ *        409:
+ *          description: Email Already Registered
+* */
+router.post("/register", register);
+
+/**
+ * @openapi
+ *
+ * /api/auth/login:
+ *   post:
+ *      summary: User Login
+ *      tags: [Auth]
+ *      requestBody: 
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                    type: object
+ *                    required: [email, password]
+ *                    properties: 
+ *                      email:
+ *                        type: string
+ *                      password: 
+ *                        type: string
+ *      responses: 
+ *        200: 
+ *          description: User Logged In
+ *        401: 
+ *          description: Invalid Credentials
+* */
+router.post("/login", login);
+
+export default router;
