dafd80a304096bf656ff9138e25929631d98429b
rishindra <rishindra.rao.16@gmail.com>
2026-05-23 22:32:18 +0530

    Create Auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export interface AuthRequest extends Request {
+  userId?: string,
+}
+
+export function requireAuth(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+    const header = req.headers['authorization']
+    if (!header || !header.startsWith('Bearer')) {
+      return res.status(401).json({ message: "Unauthorized User" });
+    }
+    const token = header.slice('Bearer'.length);
+    const secret = process.env.JWT_SECRET as string;
+    if (!secret) {
+      return res.status(500).json({message: "Internal Server Error"});
+    }
+    const payload = jwt.verify(token, secret) as { userId: string };
+    req.userId = payload.userId;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+export default requireAuth;
